11804c8043190e8f7f89ae62f80836bb8f65bad2
viennguyen24 <viennguyen@usf.edu>
2026-02-02 16:40:22 -0500

    setup scanner to read input files and generate tokens

---
+++ src/compiler/scanner/scanner.go
@@ -0,0 +1,332 @@
+﻿package scanner
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"docklett/compiler/token"
+	"docklett/compiler/util"
+	"unicode"
+	"unicode/utf8"
+)
+
+// Scanner reads the input Dockerfile code and produces a list of tokens for the parser to consume.
+type Scanner struct {
+	SourcePath string        // filepath of source code
+	SourceName string        // filename of source code
+	Source     string        // actual source code
+	start      int           // first character of current lexeme
+	current    int           // current char in source code
+	line       int           // current line in source code
+	Tokens     []token.Token // list of tokens generated
+	docklett   bool          // flag for whether we are using Docklett extensions
+}
+
+// Loads a file into the scanner and fills source metadata.
+func (s *Scanner) ReadSource(filename string) error {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	absPath, absErr := filepath.Abs(filename)
+	if absErr != nil {
+		absPath = filename
+	}
+
+	s.SourcePath = filepath.Dir(absPath)
+	s.SourceName = filepath.Base(absPath)
+	s.Source     = string(data)
+	return nil
+}
+
+// in each iteration we scan 1 token
+func (s *Scanner) ScanSource() error {
+	if s.line == 0 {
+		s.line = 1
+	}
+
+	for !s.isAtEnd() {
+		s.start = s.current // begin new lexeme
+		tokenType, literal, err := s.scanToken()
+		if err != nil {
+			return err
+		}
+		if tokenType == token.ILLEGAL {
+			continue
+		}
+		s.addToken(tokenType, literal)
+	}
+
+	s.Tokens = append(s.Tokens, token.Token{
+		Type: token.EOF,
+		Position: token.Position{
+			Line: s.line,
+			File: s.SourceName,
+			Col:  s.current + 1,
+		},
+	})
+	return nil
+}
+
+func (s *Scanner) isAtEnd() bool {
+	return s.current >= utf8.RuneCountInString(s.Source)
+}
+
+func (s *Scanner) addToken(tokenType token.TokenType, literal any) {
+	lexeme, _ := util.ReadSubstring(s.Source, s.start, s.current)
+	s.Tokens = append(s.Tokens, token.Token{
+		Type:    tokenType,
+		Lexeme:  lexeme,
+		Position: token.Position{
+			Line: s.line,
+			File: s.SourceName,
+			Col:  s.start + 1,
+		},
+		Literal: literal,
+	})
+}
+
+func (s *Scanner) advanceChar() rune {
+	r, _ := util.ReadSingleChar(s.Source, s.current)
+	s.current++
+	return r
+}
+
+func (s *Scanner) nextMatch(expected rune) bool {
+	if s.isAtEnd() {
+		return false
+	}
+	if r, _ := util.ReadSingleChar(s.Source, s.current); r != expected {
+		return false
+	}
+	s.current++
+	return true
+}
+
+func (s *Scanner) scanToken() (tokenType token.TokenType, literal any, err error) {
+	lexeme := s.advanceChar()
+	switch lexeme {
+	// skip whitespace/newlines without emitting tokens
+	case ' ', '\t', '\r':
+		return token.ILLEGAL, nil, nil
+	case '\n':
+		s.line++
+		s.docklett = false
+		return token.ILLEGAL, nil, nil
+
+	case '=':
+		if s.nextMatch('=') {
+			return token.EQUAL, nil, nil
+		}
+		return token.ASSIGN, nil, nil
+	case '+':
+		if s.nextMatch('=') {
+			return token.ADD_ASSIGN, nil, nil
+		}
+		return token.ADD, nil, nil
+	case '-':
+		if s.nextMatch('=') {
+			return token.SUB_ASSIGN, nil, nil
+		}
+		return token.SUBTRACT, nil, nil
+	case '*':
+		if s.nextMatch('=') {
+			return token.MULTI_ASSIGN, nil, nil
+		}
+		return token.MULTI, nil, nil
+	case '/':
+		if s.nextMatch('=') {
+			return token.DIV_ASSIGN, nil, nil
+		}
+		return token.DIVIDE, nil, nil
+	case '<':
+		if s.nextMatch('=') {
+			return token.LTE, nil, nil
+		}
+		return token.LESS, nil, nil
+	case '>':
+		if s.nextMatch('=') {
+			return token.GTE, nil, nil
+		}
+		return token.GREATER, nil, nil
+	case '!':
+		if s.nextMatch('=') {
+			return token.UNEQUAL, nil, nil
+		}
+		return token.NEGATE, nil, nil
+	case '&':
+		if s.nextMatch('&') {
+			return token.AND, nil, nil
+		}
+		return token.ILLEGAL, nil, fmt.Errorf("unexpected char: &")
+	case '(':
+		return token.LPAREN, nil, nil
+	case ')':
+		return token.RPAREN, nil, nil
+	case '{':
+		return token.LBRACE, nil, nil
+	case '}':
+		return token.RBRACE, nil, nil
+	case '[':
+		return token.LBRACKET, nil, nil
+	case ']':
+		return token.RBRACKET, nil, nil
+	case ':':
+		return token.COLON, nil, nil
+	case ',':
+		return token.COMMA, nil, nil
+	case '#':
+		// only ignore full line comments for now
+		// inline comments goes into the Docker instruction itself
+		return s.scanComment()
+	case '"':
+		return s.scanStringToken()
+	case '@':
+		s.docklett = true
+		return s.scanDocklettToken()
+	default:
+		if unicode.IsDigit(lexeme) {
+			return s.scanNumberToken()
+		}
+		if unicode.IsLetter(lexeme) {
+			return s.scanKeywordsAndIdentifierTokens()
+		}
+		return token.ILLEGAL, nil, fmt.Errorf("unexpected char: %q", lexeme)
+	}
+}
+
+// when encounter a #, ignore the entire line because it's a comment
+func (s *Scanner) scanComment() (tokenType token.TokenType, literal string, error error) {
+	for !s.isAtEnd() {
+		nextChar, _ := util.ReadSingleChar(s.Source, s.current)
+		if nextChar == '\n' {
+			break
+		}
+		s.advanceChar()
+	}
+	return token.ILLEGAL, "", nil
+}
+
+// when encounter a ", read every single char next until we meet the ending "
+func (s *Scanner) scanStringToken() (tokenType token.TokenType, literal string, error error) {
+	strLiteral := ""
+	for !s.isAtEnd() {
+		nextChar, _ := util.ReadSingleChar(s.Source, s.current)
+		if nextChar == '"' {
+			break
+		}
+		// read through new lines
+		if nextChar == '\n' {
+			s.line++
+		}
+		strLiteral += string(s.advanceChar())
+	}
+	if s.isAtEnd() {
+		return token.ILLEGAL, "", fmt.Errorf("unterminated string literal")
+	}
+	s.advanceChar() // consume closing " 
+	return token.STRING, strLiteral, nil
+
+}
+
+// todo: what would be the good type to handle numbers format ?
+func (s *Scanner) scanNumberToken() (tokenType token.TokenType, literal any, error error) {
+	isFloat := false
+	for !s.isAtEnd() {
+		nextChar, _ := util.ReadSingleChar(s.Source, s.current)
+		if !unicode.IsDigit(nextChar) {
+			break
+		}
+		s.advanceChar()
+	}
+	// floating number case
+	nextChar, _ := util.ReadSingleChar(s.Source, s.current)
+	if nextChar == '.' {
+		isFloat = true
+		s.advanceChar() // consume the dot
+		for !s.isAtEnd() {
+			digitChar, _ := util.ReadSingleChar(s.Source, s.current)
+			if !unicode.IsDigit(digitChar) {
+				break
+			}
+			s.advanceChar()
+		}
+	}
+	if isFloat {
+		floatLiteral, _ := strconv.ParseFloat(s.Source[s.start:s.current], 64)
+		return token.NUMBER, floatLiteral, nil
+	}
+	intLiteral, _ := strconv.Atoi(s.Source[s.start:s.current])
+	return token.NUMBER, intLiteral, nil
+}
+
+func (s *Scanner) scanDockerToken() (tokenType token.TokenType, literal any, error error) {
+	var lastNonSpace rune
+	for !s.isAtEnd() {
+		nextChar, _ := util.ReadSingleChar(s.Source, s.current)
+		if nextChar == '\n' {
+			// Dockerfile instruction may span multiple lines using \, so we check when moving into a new line are we using \ to 
+			// continue the instruction
+			continued := lastNonSpace == '\\'
+			s.advanceChar() // consume the newline
+			s.line++
+			lastNonSpace = 0
+			if continued {
+				continue
+			}
+			break
+		}
+		if nextChar != ' ' && nextChar != '\t' && nextChar != '\r' {
+			lastNonSpace = nextChar
+		}
+		s.advanceChar()
+	}
+	return token.DLINE, nil, nil
+}
+
+func (s *Scanner) scanDocklettToken() (tokenType token.TokenType, literal any, error error) {
+	text := ""
+	for !s.isAtEnd() { // read until space or non-letter/digit
+		nextChar, _ := util.ReadSingleChar(s.Source, s.current)
+		if !unicode.IsLetter(nextChar) && !unicode.IsDigit(nextChar) {
+			break
+		}
+		text += string(s.advanceChar())
+	}
+	docklettTokenType, found := token.DocklettTokenKeywords[text]
+	firstChar, _ := util.ReadSingleChar(s.Source, s.start)
+	if found {
+		return docklettTokenType, nil, nil
+	}
+	return token.ILLEGAL, nil, fmt.Errorf("unexpected Docklett token: %q", string(firstChar)+text)
+}
+
+func (s *Scanner) scanKeywordsAndIdentifierTokens() (tokenType token.TokenType, literal any, error error) {
+	text := string(s.Source[s.start])
+	for !s.isAtEnd() { // read until space or non-letter/digit
+		nextChar, _ := util.ReadSingleChar(s.Source, s.current)
+		if !unicode.IsLetter(nextChar) && !unicode.IsDigit(nextChar) {
+			break
+		}
+		text += string(s.advanceChar())
+	}
+	// if our lexeme starts with a @ we are using Docklett, prioritize Docklett keywords
+	if s.docklett {
+		if docklettTokenType, docklettFound := token.DocklettTokenKeywords[text]; docklettFound {
+			return docklettTokenType, nil, nil
+		}
+	}
+
+	// if this is a Dockerfile raw line, this entire line will be a token
+	_, found := token.DockerTokenKeywords[strings.ToUpper(text)]
+	if found {
+		return s.scanDockerToken()
+	}
+
+	return token.IDENTIFIER, text, nil
+
+} 
---
+++ src/compiler/scanner/scanner_test.go
@@ -0,0 +1,172 @@
+package scanner
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"docklett/compiler/token"
+)
+
+func tokenTypeName(t token.TokenType) string {
+	switch t {
+	case token.IDENTIFIER:
+		return "IDENTIFIER"
+	case token.STRING:
+		return "STRING"
+	case token.NUMBER:
+		return "NUMBER"
+	case token.BOOL:
+		return "BOOL"
+	case token.EQUAL:
+		return "EQUAL"
+	case token.ASSIGN:
+		return "ASSIGN"
+	case token.UNEQUAL:
+		return "UNEQUAL"
+	case token.ADD:
+		return "ADD"
+	case token.ADD_ASSIGN:
+		return "ADD_ASSIGN"
+	case token.SUBTRACT:
+		return "SUBTRACT"
+	case token.SUB_ASSIGN:
+		return "SUB_ASSIGN"
+	case token.MULTI:
+		return "MULTI"
+	case token.MULTI_ASSIGN:
+		return "MULTI_ASSIGN"
+	case token.DIVIDE:
+		return "DIVIDE"
+	case token.DIV_ASSIGN:
+		return "DIV_ASSIGN"
+	case token.NEGATE:
+		return "NEGATE"
+	case token.AND:
+		return "AND"
+	case token.OR:
+		return "OR"
+	case token.GREATER:
+		return "GREATER"
+	case token.LESS:
+		return "LESS"
+	case token.GTE:
+		return "GTE"
+	case token.LTE:
+		return "LTE"
+	case token.LPAREN:
+		return "LPAREN"
+	case token.RPAREN:
+		return "RPAREN"
+	case token.LBRACE:
+		return "LBRACE"
+	case token.RBRACE:
+		return "RBRACE"
+	case token.LBRACKET:
+		return "LBRACKET"
+	case token.RBRACKET:
+		return "RBRACKET"
+	case token.COLON:
+		return "COLON"
+	case token.COMMA:
+		return "COMMA"
+	case token.SET:
+		return "SET"
+	case token.IF:
+		return "IF"
+	case token.ELIF:
+		return "ELIF"
+	case token.ELSE:
+		return "ELSE"
+	case token.FOR:
+		return "FOR"
+	case token.IN:
+		return "IN"
+	case token.END:
+		return "END"
+	case token.TRUE:
+		return "TRUE"
+	case token.FALSE:
+		return "FALSE"
+	case token.DLINE:
+		return "DLINE"
+	case token.EOF:
+		return "EOF"
+	case token.ILLEGAL:
+		return "ILLEGAL"
+	default:
+		return fmt.Sprintf("TokenType(%d)", t)
+	}
+}
+
+func literalString(lit any) string {
+	if lit == nil {
+		return "nil"
+	}
+	switch v := lit.(type) {
+	case string:
+		return fmt.Sprintf("%q", v)
+	case int:
+		return fmt.Sprintf("%d", v)
+	case int64:
+		return fmt.Sprintf("%d", v)
+	case float64:
+		return fmt.Sprintf("%g", v)
+	case bool:
+		if v {
+			return "true"
+		}
+		return "false"
+	default:
+		return fmt.Sprintf("%v", v)
+	}
+}
+
+func scanAndPrintTokens(t *testing.T, filename, source string) {
+	t.Helper()
+
+	dir := t.TempDir()
+	path := filepath.Join(dir, filename)
+	if err := os.WriteFile(path, []byte(source), 0644); err != nil {
+		t.Fatalf("write temp source: %v", err)
+	}
+
+	var s Scanner
+	if err := s.ReadSource(path); err != nil {
+		t.Fatalf("read source: %v", err)
+	}
+	if err := s.ScanSource(); err != nil {
+		t.Fatalf("scan source: %v", err)
+	}
+
+	fmt.Printf("=== %s ===\n", filename)
+	for _, tok := range s.Tokens {
+		fmt.Printf("%-10s lexeme=%q literal=%s\n", tokenTypeName(tok.Type), tok.Lexeme, literalString(tok.Literal))
+	}
+}
+
+func TestScan_IfElse(t *testing.T) {
+	source := "@IF MODE == \"prod\"\n@ELSE\n@END\n"
+	scanAndPrintTokens(t, "if_else.dock", source)
+}
+
+func TestScan_ForList(t *testing.T) {
+	source := "@FOR pkg IN [\"curl\",\"git\",\"vim\"]\n@END\n"
+	scanAndPrintTokens(t, "for_list.dock", source)
+}
+
+func TestScan_SetAndBool(t *testing.T) {
+	source := "@SET NAME = \"docklett\"\n@IF TRUE\n@END\n"
+	scanAndPrintTokens(t, "set_bool.dock", source)
+}
+
+func TestScan_DockerfileTemplate(t *testing.T) {
+	source := "FROM python:3.12-slim\nWORKDIR /app\nCOPY requirements.txt ./\nRUN pip install --no-cache-dir -r requirements.txt\nCOPY . .\nEXPOSE 8080\nCMD [\"python\", \"app.py\"]\n"
+	scanAndPrintTokens(t, "dockerfile_template.dock", source)
+}
+
+func TestScan_DockerfileLineContinuation(t *testing.T) {
+	source := "RUN echo hello \\\n    && echo world\n"
+	scanAndPrintTokens(t, "dockerfile_line_continuation.dock", source)
+}
