bcc73a4122dfffc074f48172f3c32710849e7c02
viennguyen24 <viennguyen@usf.edu>
2026-02-02 16:37:09 -0500

    initialize basic CLI methods

---
+++ src/cli/cli.go
@@ -0,0 +1,35 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+type CommandLine struct {
+	FilePath string
+}
+
+func NewCommandLine() *CommandLine {
+	return &CommandLine{}
+}
+
+func (c *CommandLine) ParseArgs() error {
+	flag.StringVar(&c.FilePath, "file", "", "Path to Dockerfile or Docklett file")
+	flag.StringVar(&c.FilePath, "F", "", "Path to Dockerfile or Docklett file (shorthand)")
+	flag.Parse()
+	
+	if c.FilePath == "" {
+		if flag.NArg() > 0 {
+			c.FilePath = flag.Arg(0)
+		} else {
+			return fmt.Errorf("file path is required")
+		}
+	}
+	
+	if _, err := os.Stat(c.FilePath); os.IsNotExist(err) {
+		return fmt.Errorf("file does not exist: %s", c.FilePath)
+	}
+	
+	return nil
+}
