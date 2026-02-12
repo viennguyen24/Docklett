de7a363de7d4e39f31b01186cd1396b93612cf88
viennguyen24 <viennguyen@usf.edu>
2026-02-12 00:57:30 -0500

    god knows what happened

---
+++ src/compiler/error/compile.go
@@ -0,0 +1,72 @@
+package error
+
+import (
+	"docklett/compiler/token"
+	"fmt"
+)
+
+type CompileError interface {
+	error
+	GetLine() int
+	GetLocation() string
+}
+
+// ScanError represents lexical analysis errors
+type ScanError struct {
+	Line    int
+	Column  int
+	File    string
+	Message string
+}
+
+func (e *ScanError) Error() string {
+	return fmt.Sprintf("Compile Error: [line %d] %s", e.Line, e.Message)
+}
+
+func (e *ScanError) GetLine() int {
+	return e.Line
+}
+
+func (e *ScanError) GetLocation() string {
+	if e.File != "" {
+		return fmt.Sprintf("file %s, line %d, column %d", e.File, e.Line, e.Column)
+	}
+	return fmt.Sprintf("line %d, column %d", e.Line, e.Column)
+}
+
+func NewScanError(line, column int, file, message string) *ScanError {
+	return &ScanError{
+		Line:    line,
+		Column:  column,
+		File:    file,
+		Message: message,
+	}
+}
+
+// ParseError represents syntax analysis errors
+type ParseError struct {
+	Token   token.Token
+	Message string
+}
+
+func (e *ParseError) Error() string {
+	return fmt.Sprintf("Compile Error: [line %d] %s", e.Token.Position.Line, e.Message)
+}
+
+func (e *ParseError) GetLine() int {
+	return e.Token.Position.Line
+}
+
+func (e *ParseError) GetLocation() string {
+	if e.Token.Type == token.EOF {
+		return "at end"
+	}
+	return fmt.Sprintf("at '%s'", e.Token.Lexeme)
+}
+
+func NewParseError(tok token.Token, message string) *ParseError {
+	return &ParseError{
+		Token:   tok,
+		Message: message,
+	}
+}
